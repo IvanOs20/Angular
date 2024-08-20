623730131d276e9c4a309840f39898023465b2e5
Ivan Sanchez <ivansanchezj234@gmail.com>
2024-08-20 12:32:32 -0600

    add base code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'mycv2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mycv2');
+    expect(app.title).toEqual('Jesus');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mycv2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Jesus');
   });
 });
